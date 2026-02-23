ad4803c49606886f13b749f830181482a5c5ba27
.Dots <andhikahafidz.cnm.cinema@gmail.com>
2026-02-24 04:30:57 +0700

    Python Essential

---
+++ python-course/coretan.py
@@ -0,0 +1,6 @@
+kardus = []
+
+for isi in range(5):
+  kardus =+ 1
+  
+print(kardus)
---
+++ python-course2/progs/main2.py
@@ -0,0 +1,12 @@
+from sys import path
+path.append('../packages/extrapack.zip')
+
+import extra.ugly.omega as omg
+import extra.good.alpha as alp
+from extra.good.best.sigma import funS
+from extra.iota import funI
+
+print(alp.funA())
+print(omg.FunO())
+print(funS())
+print(funI())
